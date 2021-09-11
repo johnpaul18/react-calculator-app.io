be9039bfad24931032d98f6155392081eaefbbdc
John Paul Alimango <esze0909@gmail.com>
2021-09-11 14:15:07 +0800

    fixcommas

--- src/Components/Screen.js
+++ src/Components/Screen.js
@@ -1,7 +1,11 @@
 import { StyledScreen } from "./Screen.style";
 
 const Screen = ({ className, result }) => {
-  const resultWithComas = result.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  const [wholeNumber, factorial] = result.split(".");
+  const resultWithComas = `${wholeNumber.replace(
+    /\B(?=(\d{3})+(?!\d))/g,
+    ","
+  )}${factorial ? `.${factorial}` : ""}`;
 
   return (
     <div className={className}>
